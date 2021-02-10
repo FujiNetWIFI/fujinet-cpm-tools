3beb519f8f7c585d0eb038d1dfd7de92c3a1b099
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2021-02-10 11:15:26 -0600

    scaffold for bye.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fujinet-cpm-tools
 Tools to use in #FujiNet's CP/M environment.
+
+* bye - a BYE.COM program for running a typical RCP/M BBS (like RBBS, TPBBS, etc.) with #FujiNet.
---
+++ bye/src/bye.c
@@ -0,0 +1,15 @@
+/**
+ * #FujiNet BYE.COM
+ *
+ * Author:
+ *  Thom Cherryhomes <thom.cherryhomes@gmail.com>
+ *
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+  printf("#FujiNet BYE.COM v1.0\n");
+  return 0;
+}
